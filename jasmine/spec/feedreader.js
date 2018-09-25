4ffa6787a7c5233fcf7754ad6593e5b5b105fa41
ypadron <ypadron82@gmail.com>
2018-09-25 18:47:14 -0400

    test: Refactor feedreader.js formatting

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -13,106 +13,106 @@
 // var jasmineJquery = require("jasmine-jquery");
 
 $(function() {
-    //First test suite
-    describe('RSS Feeds', function() {
-        // First test
-        // allFeeds variable is defined and is not empty.
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0, null);
-        });
+	//First test suite
+	describe("RSS Feeds", function() {
+		// First test
+		// allFeeds variable is defined and is not empty.
+		it("are defined", function()  {
+			expect(allFeeds).toBeDefined();
+			expect(allFeeds.length).not.toBe(0, null);
+		});
 
-    describe("Each feed in allFeeds Object", function()  {
-      // loops through each feed in the allFeeds object and ensures it has a
-      // URL defined and is not empty.
-         it("has defined url", function()  {
-           allFeeds.forEach(function(feed)  {
-              expect(feed.url).toBeDefined();
-              expect(feed.url).not.toBe("", null);
-         });
-       });
-    });
+		describe("Each feed in allFeeds Object", function()  {
+			// loops through each feed in the allFeeds object and ensures it has a
+			// URL defined and is not empty.
+			it("has defined url", function()  {
+				allFeeds.forEach(function(feed)  {
+					expect(feed.url).toBeDefined();
+					expect(feed.url).not.toBe("");
+				});
+			});
+		});
 
-    describe("Each feed in allFeeds Object", function() {
-      // loops through each feed in the allFeeds object and ensures it has a
-      // name defined is not empty.
-         it("has defined name", function()  {
-           allFeeds.forEach(function(feed)  {
-             expect(feed.name).toBeDefined();
-             expect(feed.name).not.toBe("", null);
-           });
-         });
-      });
-    });
-
-
-      // New test suite
-      describe("The menu", function() {
-        // Ensures the menu element is hidden by default.
-         let body = document.querySelector("body");
-         let menuIcon = document.querySelector(".menu-icon-link");
-
-         it("is hidden by default", function()  {
-           // read-only property returns a live DOMTokenList -
-           // methods: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-           expect(body.classList.contains("menu-hidden")).toBe(true);
-         });
-
-      describe("The menu icon", function() {
-        // Ensures the menu changes visibility when menu icon is clicked.
-         it("changes visibility when clicked", function()  {
-           //https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/click
-           // menu displays when icon is clicked
-           menuIcon.click();
-           expect(body.classList.contains("menu-hidden")).toBe(false);
-           // menu hides when icon is clicked again
-           menuIcon.click();
-           expect(body.classList.contains("menu-hidden")).toBe(true);
-         });
-      });
-  });
+		describe("Each feed in allFeeds Object", function() {
+			// loops through each feed in the allFeeds object and ensures it has a
+			// name defined is not empty.
+			it("has defined name", function()  {
+				allFeeds.forEach(function(feed)  {
+					expect(feed.name).toBeDefined();
+					expect(feed.name).not.toBe("");
+				});
+			});
+		});
+	});
 
 
-      // New test suite
-      describe("Initial Entries", function()  {
-        // Ensures an .entry element within the .feed container when loadFeed
-        // function is called and completes its work.
-         let container = document.querySelector(".feed");
+	// New test suite
+	describe("The menu", function() {
+		// Ensures the menu element is hidden by default.
+		let body = document.querySelector("body");
+		let menuIcon = document.querySelector(".menu-icon-link");
 
-         // Jasmine specific global function (async) runs setup (loadFeed) before each
-         // spec in the describe (test) in which it is called.
-         beforeEach(function(done)  {
-           loadFeed(0, done);
-         });
+		it("is hidden by default", function()  {
+			// read-only property returns a live DOMTokenList -
+			// methods: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+			expect(body.classList.contains("menu-hidden")).toBe(true);
+		});
 
-         it("verifies feed contains an entry after loadFeed function executes", function(done) {
-           expect(container.children.length).toBeGreaterThan(0);
-           done();
-         });
-      });
+		describe("The menu icon", function() {
+			// Ensures the menu changes visibility when menu icon is clicked.
+			it("changes visibility when clicked", function()  {
+				//https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/click
+				// menu displays when icon is clicked
+				menuIcon.click();
+				expect(body.classList.contains("menu-hidden")).toBe(false);
+				// menu hides when icon is clicked again
+				menuIcon.click();
+				expect(body.classList.contains("menu-hidden")).toBe(true);
+			});
+		});
+	});
 
 
-    // New test suite
-    describe("New Feed Selection", function() {
-      // Ensures content actually changes when a new feed is loaded by loadFeed function.
-       let firstFeed;
-       let otherFeeds;
+	// New test suite
+	describe("Initial Entries", function()  {
+		// Ensures an .entry element within the .feed container when loadFeed
+		// function is called and completes its work.
+		let container = document.querySelector(".feed");
 
-      beforeEach(function(done)  {
-        loadFeed(1, function()  {
-          otherFeeds = document.querySelectorAll(".feed .entry");
-          console.log(otherFeeds[0].innerText);
+		// Jasmine specific global function (async) runs setup (loadFeed) before each
+		// spec in the describe (test) in which it is called.
+		beforeEach(function(done)  {
+			loadFeed(0, done);
+		});
 
-        loadFeed(0, function()  {
-          firstFeed = document.querySelectorAll(".feed .entry");
-          done();
-          console.log(firstFeed[0].innerText);
-          });
-        });
-      });
+		it("verifies feed contains an entry after loadFeed function executes", function(done) {
+			expect(container.children.length).toBeGreaterThan(0);
+			done();
+		});
+	});
 
-        it("checks for content change on new entry load in the feed", function() {
-           expect(otherFeeds[0].innerText).not.toBe(firstFeed[0].innerText);
-         });
-      });
+
+	// New test suite
+	describe("New Feed Selection", function() {
+		// Ensures content actually changes when a new feed is loaded by loadFeed function.
+		let firstFeed;
+		let otherFeeds;
+
+		beforeEach(function(done)  {
+			loadFeed(1, function()  {
+				otherFeeds = document.querySelectorAll(".feed .entry");
+				console.log(otherFeeds[0].innerText);
+
+				loadFeed(0, function()  {
+					firstFeed = document.querySelectorAll(".feed .entry");
+					done();
+					console.log(firstFeed[0].innerText);
+				});
+			});
+		});
+
+		it("checks for content change on new entry load in the feed", function() {
+			expect(otherFeeds[0].innerText).not.toBe(firstFeed[0].innerText);
+		});
+	});
 }());
